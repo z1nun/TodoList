31501ff4e4468f21b234657dca37f653d933b933
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-04 02:33:50 +0900

    Docs:handleSubmit함수, JSX작성

--- src/component/TodoCreate.js
+++ src/component/TodoCreate.js
@@ -1,29 +1,24 @@
-import useFetch from "../utill/useFetch";
 import { fetchCreate } from "../utill/api";
 import { useState } from "react";
 
 const TodoCreate = () => {
   const [title, setTitle] = useState("");
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = { title };
     fetchCreate("http://localhost:3001/todo/", data);
   };
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          required
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="일정을 추가해주세요."
-        ></input>
-        <button>submit</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        required
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="일정을 입력해주세요."
+      ></input>
+      <button>submit</button>
+    </form>
   );
 };
 
