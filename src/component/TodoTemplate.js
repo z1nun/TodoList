019e8568a36ce679f92b300243e130b2c829c673
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-04 02:35:09 +0900

    Docs:margin-top 수정

--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -5,12 +5,11 @@ const Todotemplateblock = styled.div`
   width: 512px;
   height: 768px;
   background-color: white;
-
   position: relative;
   border-radius: 16px;
   box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.3);
   margin: 0 auto;
-  margin-top: 200;
+  margin-top: 68px;
   margin-bottom: 32px;
   display: flex;
   flex-direction: column;
