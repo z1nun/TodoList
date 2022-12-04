b12975a4e3a56810c1552e888d3cdca3cad35c9f
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-04 20:30:18 +0900

    Docs: 코드 수정 및 조건부렌더링

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,10 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import { useState } from "react";
+
+const TodolistStyle = styled.div`
+  color: red;
+`;
 
 const TodoItem = (props) => {
-  console.log(props.todo);
+  console.log(props);
+  const [isCheck, setCheck] = useState(false);
+  const handleCheck = () => {
+    setCheck(!isCheck);
+  };
   return (
     <div>
-      {props.todo && props.todo.map((el) => <li key={el.id}>{el.title}</li>)}
+      <div onClick={handleCheck}>
+        {isCheck === true ? (
+          <TodolistStyle>{props.todo}</TodolistStyle>
+        ) : (
+          props.todo
+        )}
+      </div>
     </div>
   );
 };
