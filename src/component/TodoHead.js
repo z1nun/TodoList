5472fd354d109a241ad98e647a22b678d8e986aa
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-04 23:26:48 +0900

    Feat: 명언 나타내기

--- src/component/TodoHead.js
+++ src/component/TodoHead.js
@@ -1,2 +1,15 @@
-const TodoHead = () => {};
+import { logRoles } from "@testing-library/react";
+import { fetchCreate } from "../utill/api";
+const TodoHead = (props) => {
+  console.log(props.quotes);
+
+  const randomNum = Math.floor(Math.random() * 100);
+  return (
+    <div>
+      <h1>TodoList</h1>
+      {props.quotes && <div>{props.quotes[randomNum].quote}</div>}
+      {props.quotes && <div>{props.quotes[randomNum].author}</div>}
+    </div>
+  );
+};
 export default TodoHead;
