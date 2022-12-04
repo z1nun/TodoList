485381f597e1bb71f30cbfe5598e7f3052b46ba3
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-04 20:29:37 +0900

    Docs: TodoItem컴포넌트 Mapping 코드 작성

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useState } from "react";
 import TodoCreate from "./TodoCreate";
 import TodoItem from "./TodoItem";
+import styled from "styled-components";
 
 const TodoList = (props) => {
-  console.log(props.todo);
   return (
     <div>
       <TodoCreate />
-      <TodoItem todo={props.todo} />
+      {props.todo &&
+        props.todo.map((el) => <TodoItem key={el.id} todo={el.title} />)}
     </div>
   );
 };
