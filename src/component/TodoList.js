279eca62204be1a819cc7bd33ac5c4e1396bd4dd
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-12 21:05:59 +0900

    Docs:불필요한 코드 삭제, 컴포넌트 이름 알기쉽게 만들기

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -2,23 +2,29 @@ import React from "react";
 import TodoCreate from "./TodoCreate";
 import TodoItem from "./TodoItem";
 import styled from "styled-components";
+
 const ListStyle = styled.div`
   font-size: 1.25rem;
   display: flex;
-  justify-content: center;
-  margin-top: -250px;
+  flex-direction: column;
+  width: 668px;
+  height: 400px;
+  padding: 20px;
+  background-color: rgb(0, 0, 0, 0.4);
+  border-radius: 20px;
 `;
 
 const TodoList = (props) => {
-  console.log(`list`, props.todo);
   return (
-    <ListStyle>
-      <div>
-        <TodoCreate />
-        {props.todo &&
-          props.todo.map((el) => <TodoItem key={el.id} todo={el} />)}
-      </div>
-    </ListStyle>
+    <div>
+      <ListStyle>
+        <div>
+          <TodoCreate />
+          {props.todo &&
+            props.todo.map((el) => <TodoItem key={el.id} todo={el} />)}
+        </div>
+      </ListStyle>
+    </div>
   );
 };
 
