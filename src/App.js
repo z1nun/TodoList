16ad054e74d4293c642226b59c9929af2c5a60a0
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-12 21:07:14 +0900

    Docs: 구글검색창 placeholder함..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import TodoTemplate from "./component/TodoTemplate.js";
 
-import { createGlobalStyle } from "styled-components";
 import useFetch from "./utill/useFetch.js";
 import "./App.css";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-  background-color: #e9ecef;
-  /* position: relative; */
-  }
-
-`;
 
 function App() {
   const { data: todo } = useFetch("http://localhost:3001/todo");
   const { data: quotes } = useFetch("http://localhost:3001/quotes");
 
+  const $input = document.getElementById("gsc-i-id1");
+  $input && $input.setAttribute("placeholder", "Google 검색");
+
   return (
     <>
-      <GlobalStyle />
       <TodoTemplate todo={todo} quotes={quotes} />
     </>
   );
