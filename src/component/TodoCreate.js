3ca890a64f78bd930adfdd9716bbc43356a1d7a8
강진원 <jinwon@gangjin-won-ui-MacBookAir.local>
2022-12-12 21:03:55 +0900

    Docs: TodoCreate.js 완성 전단계(불필요한 코드 삭제하기)

--- src/component/TodoCreate.js
+++ src/component/TodoCreate.js
@@ -1,10 +1,6 @@
 import { fetchCreate } from "../utill/api";
 import { useState } from "react";
 import styled from "styled-components";
-const P = styled.p`
-  display: flex;
-  justify-content: center;
-`;
 
 const Input = styled.input`
   display: flex;
@@ -12,13 +8,25 @@ const Input = styled.input`
   margin: 20px auto;
   background-color: rgb(255, 255, 255, 0);
   border-color: rgb(255, 255, 255, 0);
-  border-bottom-color: black;
-  width: 400px;
+  border-bottom-color: white;
+  width: 540px;
   height: 40px;
+  text-align: center;
+  font-weight: 600;
+  font-size: 1.25rem;
+  color: white;
+  text-shadow: #828282 1px 0 1px;
+  outline: none;
+  margin-top: -20px;
+  ::placeholder {
+    color: white;
+    font-size: 1.25rem;
+  }
 `;
 
 const TodoCreate = () => {
   const [title, setTitle] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = { title };
@@ -26,13 +34,13 @@ const TodoCreate = () => {
   };
   return (
     <div>
-      <P>오늘의 일정을 적어주세요!</P>
       <form onSubmit={handleSubmit}>
         <Input
           type="text"
           required
           value={title}
           onChange={(e) => setTitle(e.target.value)}
+          placeholder="Write a to do!"
         ></Input>
       </form>
     </div>
